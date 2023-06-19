c8b427e28d4b29e4901059e9f3b3eaec94865d61
Saitejahr <98202897+Saitejahr@users.noreply.github.com>
2023-06-19 21:57:36 +0530

    first commit

---
+++ src/components/Common/Loader/index.js
@@ -0,0 +1,10 @@
+import { CircularProgress } from '@mui/material'
+import './styles.css'
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <CircularProgress />
+    </div>
+  )
+}
+export default Loader
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import Header from '../components/Common/Header'
+import MainComponent from '../components/LandingPage/MainComponent'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <MainComponent />
+    </div>
+  )
+}
+export default HomePage
